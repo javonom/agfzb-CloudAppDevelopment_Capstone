8f7d305480b37a4f93b94b4fc0e48934816919eb
javonom <javonminott21@gmail.com>
2022-03-02 19:15:12 -0500

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.db import models
 from django.utils.timezone import now
 
@@ -9,7 +10,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=35, primary_key=True)
+    descript = models.TextField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -21,7 +27,70 @@ from django.utils.timezone import now
 # - __str__ method to print a car make object
 
 
+class CarModel(models.Model):
+    name = models.CharField(max_length=35, primary_key=True)
+    id = models.CharField(max_length=20)
+    type = models.CharField(max_length=10)
+    year = models.DateField(null=True)
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Dealership
+        self.dealership = dealership
+        # Dealer name
+        self.name = name
+        # Dealer purchase
+        self.purchase = purchase
+        # Dealer review
+        self.review = review
+        # purchase_date
+        self.purchase_date = purchase_date
+        # car_make
+        self.car_make = car_make
+        # car_model
+        self.car_model = car_model
+        # car_year
+        self.car_year = car_year
+        # sentiment
+        self.sentiment = sentiment
+        # id
+        self.id = id
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, CarMake, CarModel
+from .restapis import get_dealers_from_cf, get_request, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -28,11 +28,46 @@ def contact(request):
     return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
+
+
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            # If not, return to login page again
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
 # ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+
+
+def logout_request(request):
+    # Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
+    logout(request)
+    # Redirect user back to course list view
+    return redirect('djangoapp:index')
+
+# Create a `registration_request` view to handle sign up request
+
+
+def registration_request(request):
+    return render(request, 'djangoapp/registration.html')
+
 # ...
 
 # Create a `registration_request` view to handle sign up request
@@ -51,7 +86,12 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
-
+def get_dealer_details(request, dealer_id):
+    return HttpResponse(dealer_id)
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
+
+
+def add_review(request, dealer_id):
+    pass
